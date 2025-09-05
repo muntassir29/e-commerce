4dff8491bb4e59a455572970c05a18574652c9e9
montassir <montassirchehboun@gmail.com>
2025-09-05 04:04:52 +0200

    update gradientbutton

--- frontend/src/components/GradientButton.jsx
+++ frontend/src/components/GradientButton.jsx
@@ -1,14 +1,3 @@
-// export default function GradientButton({ children, type = "button", onClick }) {
-//   return (
-//     <button
-//       type={type}
-//       onClick={onClick}
-//       className="px-6 py-3 rounded-xl font-semibold text-white bg-gradient-to-r from-purple-500 to-indigo-600 hover:from-purple-600 hover:to-indigo-700 shadow-md transition"
-//     >
-//       {children}
-//     </button>
-//   );
-// }
 
 import { motion } from "framer-motion";
 
