bd5235eec5d5352601974d6af516017cd220eed8
montassir <montassirchehboun@gmail.com>
2025-09-06 03:19:00 +0200

    update Home and App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Product from "./pages/Product";
-// import Checkout from "./pages/Checkout";
-// import Success from "./pages/Success";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="flex flex-col min-h-screen">
-//         <Navbar />
-//         <main className="flex-1 pt-32">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/product/:id" element={<Product />} />
-//             <Route path="/checkout" element={<Checkout />} />
-//             <Route path="/success" element={<Success />} />
-//             <Route path="/about" element={<About />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/product/:id" element={<Product />} />
-//           </Routes>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
